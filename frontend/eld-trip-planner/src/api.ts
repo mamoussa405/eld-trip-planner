00f11d187fc0dbce658bb91c11401a18532d5fa3
Mohamed Amoussaoui <mamoussa@Mohameds-MacBook-Pro.local>
2025-10-29 20:00:50 +0100

    Updated vite url

--- frontend/eld-trip-planner/src/api.ts
+++ frontend/eld-trip-planner/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { RouteData } from './types'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE ?? 'http://localhost:8000'
+const API_BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:8000'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
